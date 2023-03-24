c19ba8035ee4f7c1a84d0c7da39b09baf81f4e72
AbelGSIG <abel.gonzalez@sigob.com.mx>
2023-03-23 20:51:05 -0700

    Update api.js

--- api.js
+++ api.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 const fetchweatherData = async () => {
     const { data } = await axios.get(
-        'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={cf44e93b52d564987f483061cf006652}'
+        'http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=cf44e93b52d564987f483061cf006652'
     );
     console.log(data);
 };
